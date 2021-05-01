98f8b83b9b56df63a5c4f6e57f85446df3a295d1
Romain Cendre <romain.cendre@gmail.com>
2021-05-01 22:54:49 +0200

    Add change to setup

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ import setuptools
 version = '0.1'
 
 setup(name='keras-gan',
-      version=version,
-      url='https://github.com/eriklindernoren/Keras-GAN',
-      license='MIT',
-      include_package_data=True,
+      author='Cendre Romain',
+      author_email='romain.cendre@gmail.com',
+      version='0.1dev',
       packages=setuptools.find_packages())
